198d217cea4724a4688eeafed0ed556f2bc27f3c
Jonathan Warren <git@jonwarren.org>
2015-01-02 16:13:27 -0500

    incremented version to 1.1

--- chain/__init__.py
+++ chain/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-SDKVersion = "1.0"
+SDKVersion = "1.1"
 APIVersion = "v2"
 
 from .lib import Chain
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='chain-sdk',
-    version='1.0',
+    version='1.1',
     author='Jonathan Warren',
     author_email='jonathan@coinapex.com',
     packages=['chain'],
